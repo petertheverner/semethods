c066503003558aaa3c54cbbdc003e6a5e8a839a2
The-Liam-Blair <blair.liam@hotmail.co.uk>
2021-02-24 22:07:02 +0000

    - Added SQL file with it's Dockerfile - Added docker-compose file and added functionality to allow the main application to connect to the database.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,54 @@
 package com.napier.sem;
 
+import javax.swing.plaf.nimbus.State;
+import javax.xml.transform.Result;
+import java.sql.*;
+
     public class App
     {
+        private Connection con = null;
+
+        public void Connect() {
+
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+            }
+            catch (ClassNotFoundException e)
+            {
+                System.out.println("SQL driver could not be loaded.");
+                System.exit(-1);
+            }
+
+            int tries = 10;
+
+            for (int i = 0; i < tries; i++) {
+                System.out.println("Connecting to database...");
+
+                try
+                {
+                    Thread.sleep(30000);
+                    con = DriverManager.getConnection("jdbc:mysql://DB:3306/world?useSSL=false", "root", "example");
+                    System.out.println("Succesful connection to the database.");
+                    break;
+                }
+
+                catch (SQLException e)
+                {
+                    System.out.println("Failed to connect to database on attempt number" + i);
+                    System.out.println(e.getMessage());
+                }
+
+                catch (InterruptedException e)
+                {
+                    System.out.println("System interrupted");
+                }
+            }
+        }
+
         public static void main(String[] args)
         {
-            System.out.println("Boo yah!");
+            App a = new App();
+            a.Connect();
         }
-    }
+   }
